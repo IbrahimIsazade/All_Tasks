4d94377ed7ffda1c01a03b0088b1f27d28415eb4
IbrahimIsazade <ibrahimhi@code.edu.az>
2023-11-20 22:04:28 +0400

    Added Task-2

---
+++ Task-2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating Variables
+            float a, b;
+            string name, surename, fullname;
+
+            // Body part
+            name = "name";
+            surename = "surename";
+            fullname = $"{name} {surename}";
+            Console.WriteLine(fullname);
+
+            a = 18;
+            b = 5;
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.ReadKey();
+        }
+    }
+}
