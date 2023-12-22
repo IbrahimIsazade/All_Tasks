ef8feea65516ed84e10748d22afda8c3da7194de
IbrahimIsazade <ibrahimhi@code.edu.az>
2023-12-22 12:45:35 +0400

    Updated Task-7
    Finished

--- Task-7/Program.cs
+++ Task-7/Program.cs
@@ -16,7 +16,10 @@
 
             int c = ReadInt("Enter the `C`: ");
             int[] x = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 }, 
-                  y = { 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+                  y = { 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 }, 
+                  z = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 },
+                  rX = new int[0];
+
             Part1(array, c);
             Part2(array, c);
             Part3(array, c);
@@ -195,13 +198,119 @@
             Console.WriteLine($"\n=================================");
         }
 
-        static void Part9(int[] arrX, int[] arrY)
+        static void Part9(int[] arr)
         {
-            // PASTE Q/T HERE
+            // 9. 25 elementli massivin ilk 8 elementini kvadrata yukseltmeli,
+            // qalanini ise 4-e bolub,yeni iki massiv duzeltmeli
             Console.WriteLine("Part 9:");
-            
+            int[] resPow = new int[8], resDivide = new int[17]; 
+
+            for (int i = 0; i < 8; i++)
+            {
+                resPow[i] = arr[i]*arr[i];
+            }
+            int j = 0;
+            for (int i = 8; i < arr.Length; i++)
+            {
+                resDivide[i] = arr[i]/4;
+                j++;
+            }
+            Console.WriteLine(resPow);
+            Console.WriteLine(resDivide);
+            Console.WriteLine($"\n=================================");
+        }
+
+        static void Part10(int[] arr)
+        {
+            // 10. X(n) massivinden ancaq menfi elementlerin maximum elementi tapin
+            Console.WriteLine("Part 10:");
+            int[] minNums = new int[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    Array.Resize(ref minNums, minNums.Length);
+                    minNums[i] = arr[i];
+                }
+            }
+            int max = minNums[0];
+            foreach (var item in minNums)
+            {
+                if (item > max)
+                {
+                    max = item;
+                }
+            }
+
+            Console.WriteLine(max);
 
             Console.WriteLine($"\n=================================");
+        }
+
+        static void Part11(int[] arr)
+        {
+            // 11. X(n) massivinin elementlerini random olaraq 0-100 arasi
+            // ededlerle doldurub,sonra ise azalma sirasina gore duzmeli
+            Console.WriteLine("Part 11:");
+            SetRandomElements(ref arr, 10);
+
+            int num;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] < arr[j])
+                    {
+                        num = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = num;
+                    }
+                }
+            }
+
+            Console.WriteLine($"\n=================================");
+        }
+
+        static void Part12(int[] arr)
+        {
+            // 12. X(20) massivinin ilk yarisinin ededi
+            // ortasini ikinci yarisinin hendesi ortasini tapmaq
+            Console.WriteLine("Part 12:");
+            SetRandomElements(ref arr, 20);
+            int resEdedi = 0, resHendesi = 0;
+
+            for (int i = 0; i < arr.Length/2; i++)
+            {
+                resEdedi += arr[i];
+            }
+            resEdedi /= arr.Length/2;
+
+            for (int i = arr.Length/2; i < arr.Length; i++)
+            {
+                resHendesi *= arr[i];
+            }
+            resHendesi = Convert.ToInt32(Math.Pow(resHendesi, 1/(arr.Length/2)));
+
+            Console.WriteLine(resEdedi);
+            Console.WriteLine(resHendesi);
+
+            Console.WriteLine($"\n=================================");
+        }
+
+
+        static void SetRandomElements(ref int[] x, int elementCount)
+        {
+            Random rCount = new Random();
+            Random rElement = new Random();
+            int count = rCount.Next(1, elementCount+1), element;
+
+            for (int i = 0; i < count; i++)
+            {
+                Array.Resize(ref x, x.Length+1);
+                element = rElement.Next(1, 101);
+                x[i] = element;
+            }
         }
 
         static int ReadInt(string text)
