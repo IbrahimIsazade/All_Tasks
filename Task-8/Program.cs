bd07b5f1f4d7ea33565c4cb7885edffa4bf52841
IbrahimIsazade <ibrahimhi@code.edu.az>
2023-12-28 09:43:24 +0400

    Update Task-8

--- Task-8/Program.cs
+++ Task-8/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Diagnostics.Metrics;
+using System.Globalization;
+using System.Text;
 using System;
 
 namespace Task_8
@@ -7,12 +9,44 @@ namespace Task_8
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.Unicode;
+
             Part1(2);
             Part2(4, 2);
             Part3(207);
             Part4(22);
             Part5(22);
             Part6();
+
+            // Part 7
+            CultureInfo culture = new CultureInfo("az");
+            DateTime now = DateTime.Now;
+            string add ="";
+            switch (now.Year%10)
+            {
+                case 6:
+                    add = "-cı il";
+                    break;
+                case 1:
+                case 2:
+                case 5:
+                case 7:
+                case 8:
+                    add = "-ci il";
+                    break;
+                case 0:
+                case 9:
+                    add = "-cu il";
+                    break;
+                case 3:
+                case 4:
+                    add = "-cü il";
+                    break;
+            }
+            culture.DateTimeFormat.LongDatePattern = "d MMMM yyyy"+add;
+            Thread.CurrentThread.CurrentCulture = culture;
+            Console.WriteLine(now.ToLongDateString());
+
         }
 
         static void Part1(int num)
@@ -54,30 +88,27 @@ namespace Task_8
 
         static void Part4(int num)
         {
-            string[] numArr = num.ToString().Split("");
-            string last;
-            last = numArr[0];
-            foreach (string letter in numArr)
+            if (num/10 == num%10)
             {
-                if (letter == last)
-                {
-                    Console.WriteLine("They're not same !");
-                    return;
-                }
+                Console.WriteLine("Beli");
+            }
+            else
+            {
+                Console.WriteLine("Xeyr");
             }
         }
 
         static void Part5(int num)
         {
-            string[] numArr = num.ToString().Split("");
-            string last;
-            last = numArr[0];
-            foreach (string letter in numArr)
+            if (num >= 10 && num < 100)
             {
-                if (letter == last)
+                if ((num/10 + num%10) % 2 == 0)
                 {
-                    Console.WriteLine("They're not same !");
-                    return;
+                    Console.WriteLine((num/10) * (num%10));
+                }
+                else
+                {
+                    Console.WriteLine("ədəddə tək rəqəm mövcuddu");
                 }
             }
         }
@@ -113,11 +144,12 @@ namespace Task_8
                 Array.Resize(ref students, students.Length + 1);
                 students[students.Length - 1] = student;
             } while (Console.ReadKey().Key == ConsoleKey.Enter);
-            Console.WriteLine("Students: ");
+            Console.WriteLine("\nStudents: ");
             foreach (var item in students)
             {
                 Console.WriteLine($"Name: {item.name}, Date of birth {item.dateOfBirth}");
             }
         }
+
     }
 }
